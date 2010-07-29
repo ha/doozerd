fa2e72592d4d801469d667fc132a742b5ef7f774
Blake Mizerany <blake.mizerany@gmail.com>
2010-07-28 19:37:59 -0700

    start of proto

---
+++ proto/proto.go
@@ -0,0 +1,71 @@
+package proto
+
+import(
+  "bufio"
+  "strconv"
+  "strings"
+  "os"
+)
+
+type Request struct {
+  Parts [][]byte
+  Err os.Error
+}
+
+var (
+  ProtocolError = os.NewError("multi bulk protocol error")
+)
+
+func scanNumber(data *bufio.Reader, after byte) (n uint64, err os.Error) {
+
+  for {
+    var c byte
+
+    c, err = data.ReadByte()
+    if err != nil { return }
+
+    switch c {
+    default:
+      err = ProtocolError
+      return
+    case '\r', '\n':
+      continue
+    case after:
+      sn, err := data.ReadString('\n')
+      if err != nil { return }
+
+      return strconv.Btoui64(strings.TrimSpace(sn), 10)
+    }
+  }
+
+  panic("This should never be reached!")
+}
+
+func Scan(data *bufio.Reader, ch chan *Request) {
+
+  for {
+    var err os.Error
+
+    count, err := scanNumber(data, '*')
+    if err != nil { ch <- &Request{Err:err} }
+
+    parts := make([][]byte, count)
+
+    for count > 0 {
+      size, err := scanNumber(data, '$')
+      if err != nil { ch <- &Request{Err:err} }
+
+      // Read the data
+      bytes := make([]byte, size)
+      _, err = data.Read(bytes)
+      if err != nil { ch <- &Request{Err:err} }
+
+      parts[len(parts) - int(count)] = bytes
+
+      count--
+    }
+
+    ch <- &Request{Parts:parts}
+  }
+
+}
---
+++ proto/proto_test.go
@@ -0,0 +1,103 @@
+package proto
+
+import(
+  "bufio"
+  "reflect"
+  "bytes"
+  "testing"
+)
+
+/* Assertions */
+func assertEqual(t *testing.T, expected, result interface{}, message string) {
+  if ! reflect.DeepEqual(expected, result) {
+    t.Fatalf("expected <%#v> but got <%#v> (%s)", expected, result, message)
+  }
+}
+
+/* Helpers */
+func setupPipe(chunks ... string) (buf *bufio.Reader, ch chan *Request) {
+  ch = make(chan *Request)
+  data := new(bytes.Buffer);
+  buf = bufio.NewReader(data);
+  for _, chunk := range(chunks) {
+    data.WriteString(chunk)
+  }
+  return
+}
+
+func TestOneRequestOnePart(t *testing.T) {
+  buf, ch := setupPipe("*1\r\n$3\r\nfoo\r\n")
+  go Scan(buf, ch);
+
+  var req *Request
+  req = <- ch
+
+  assertEqual(t, 1, len(req.Parts), "")
+  assertEqual(t, "foo", string(req.Parts[0]), "")
+  if req.Err != nil { t.Fatalf("expected %#v to be nil", req.Err) }
+}
+
+func TestTwoRequestsOnePart(t *testing.T) {
+  buf, ch := setupPipe(
+    "*1\r\n$3\r\nfoo\r\n",
+    "*1\r\n$3\r\nbar\r\n",
+  )
+  go Scan(buf, ch);
+
+  var req *Request
+  req = <- ch
+
+  assertEqual(t, 1, len(req.Parts), "")
+  assertEqual(t, "foo", string(req.Parts[0]), "")
+  if req.Err != nil {
+    t.Fatalf("expected %#v to be nil", req.Err)
+  }
+
+  req = <- ch
+
+  assertEqual(t, 1, len(req.Parts), "")
+  assertEqual(t, "bar", string(req.Parts[0]), "")
+  if req.Err != nil { t.Fatalf("expected %#v to be nil", req.Err) }
+}
+
+func TestOneRequestTwoParts(t *testing.T) {
+  buf, ch := setupPipe("*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n")
+  go Scan(buf, ch);
+
+  var req *Request
+  req = <- ch
+
+  assertEqual(t, 2, len(req.Parts), "")
+  assertEqual(t, "foo", string(req.Parts[0]), "")
+  assertEqual(t, "bar", string(req.Parts[1]), "")
+  if req.Err != nil { t.Fatalf("expected %#v to be nil", req.Err) }
+}
+
+func TestScanNumber(t *testing.T) {
+
+}
+
+func TestOneRequestWithError(t *testing.T) {
+  stuff := []string{
+    "a",
+    "$a",
+    "$1a",
+    "$1",
+    "*a",
+    "*1a",
+    "*1\na",
+    "*1\n$a",
+    "*1\n$1a",
+  }
+
+  for _, data := range(stuff) {
+    buf, ch := setupPipe(data)
+    go Scan(buf, ch);
+
+    var req *Request
+    req = <- ch
+
+    assertEqual(t, ProtocolError, req.Err, data)
+    if req.Parts != nil { t.Fatalf("expected %#v to be nil", req.Err) }
+  }
+}
