1c4f908d47d94da41afd7c15ce9c9d7b8da922ef
Blake Mizerany <blake.mizerany@gmail.com>
2010-08-04 14:25:05 -0700

    gofmt proto

--- pkg/proto/proto.go
+++ pkg/proto/proto.go
@@ -1,70 +1,80 @@
 package proto
 
-import(
-  "bufio"
-  "strconv"
-  "strings"
-  "os"
+import (
+	"bufio"
+	"strconv"
+	"strings"
+	"os"
 )
 
 type Request struct {
-  Parts [][]byte
-  Err os.Error
+	Parts [][]byte
+	Err   os.Error
 }
 
 var (
-  ProtocolError = os.NewError("multi bulk protocol error")
+	ProtocolError = os.NewError("multi bulk protocol error")
 )
 
 func scanNumber(data *bufio.Reader, after byte) (n uint64, err os.Error) {
 
-  for {
-    var c byte
+	for {
+		var c byte
 
-    c, err = data.ReadByte()
-    if err != nil { return }
+		c, err = data.ReadByte()
+		if err != nil {
+			return
+		}
 
-    switch c {
-    default:
-      err = ProtocolError
-      return
-    case '\r', '\n':
-      continue
-    case after:
-      var sn string
-      sn, err = data.ReadString('\n')
-      if err != nil { return }
+		switch c {
+		default:
+			err = ProtocolError
+			return
+		case '\r', '\n':
+			continue
+		case after:
+			var sn string
+			sn, err = data.ReadString('\n')
+			if err != nil {
+				return
+			}
 
-      return strconv.Btoui64(strings.TrimSpace(sn), 10)
-    }
-  }
+			return strconv.Btoui64(strings.TrimSpace(sn), 10)
+		}
+	}
 
-  panic("This should never be reached!")
+	panic("This should never be reached!")
 }
 
 func Scan(data *bufio.Reader, ch chan *Request) {
 
-  for {
-    count, err := scanNumber(data, '*')
-    if err != nil { ch <- &Request{Err:err} }
+	for {
+		count, err := scanNumber(data, '*')
+		if err != nil {
+			ch <- &Request{Err: err}
+		}
 
-    parts := make([][]byte, count)
+		parts := make([][]byte, count)
 
-    for count > 0 {
-      size, err := scanNumber(data, '$')
-      if err != nil { ch <- &Request{Err:err} }
+		for count > 0 {
+			size, err := scanNumber(data, '$')
+			if err != nil {
+				ch <- &Request{Err: err}
+			}
 
-      // Read the data
-      bytes := make([]byte, size)
-      _, err = data.Read(bytes)
-      if err != nil { ch <- &Request{Err:err} }
+			// Read the data
+			bytes := make([]byte, size)
+			_, err = data.Read(bytes)
+			if err != nil {
+				ch <- &Request{Err: err}
+			}
 
-      parts[len(parts) - int(count)] = bytes
+			parts[len(parts)-int(count)] = bytes
 
-      count--
-    }
+			count--
+		}
 
-    ch <- &Request{Parts:parts}
-  }
+		ch <- &Request{Parts: parts}
+	}
 
 }
--- pkg/proto/proto_test.go
+++ pkg/proto/proto_test.go
@@ -1,77 +1,83 @@
 package proto
 
-import(
-  "bufio"
-  "reflect"
-  "bytes"
-  "fmt"
-  "testing"
+import (
+	"bufio"
+	"reflect"
+	"bytes"
+	"fmt"
+	"testing"
 )
 
 /* Assertions */
 func assertEqual(t *testing.T, expected, result interface{}, message string) {
-  if ! reflect.DeepEqual(expected, result) {
-    t.Fatalf("expected <%#v> but got <%#v> (%s)", expected, result, message)
-  }
+	if !reflect.DeepEqual(expected, result) {
+		t.Fatalf("expected <%#v> but got <%#v> (%s)", expected, result, message)
+	}
 }
 
 /* Helpers */
-func setupPipe(chunks ... string) (buf *bufio.Reader, ch chan *Request) {
-  ch = make(chan *Request)
-  data := new(bytes.Buffer);
-  buf = bufio.NewReader(data);
-  for _, chunk := range(chunks) {
-    data.WriteString(chunk)
-  }
-  return
+func setupPipe(chunks ...string) (buf *bufio.Reader, ch chan *Request) {
+	ch = make(chan *Request)
+	data := new(bytes.Buffer)
+	buf = bufio.NewReader(data)
+	for _, chunk := range chunks {
+		data.WriteString(chunk)
+	}
+	return
 }
 
 func TestOneRequestOnePart(t *testing.T) {
-  buf, ch := setupPipe("*1\r\n$3\r\nfoo\r\n")
-  go Scan(buf, ch);
+	buf, ch := setupPipe("*1\r\n$3\r\nfoo\r\n")
+	go Scan(buf, ch)
 
-  var req *Request
-  req = <- ch
+	var req *Request
+	req = <-ch
 
-  assertEqual(t, 1, len(req.Parts), "")
-  assertEqual(t, "foo", string(req.Parts[0]), "")
-  if req.Err != nil { t.Fatalf("expected %#v to be nil", req.Err) }
+	assertEqual(t, 1, len(req.Parts), "")
+	assertEqual(t, "foo", string(req.Parts[0]), "")
+	if req.Err != nil {
+		t.Fatalf("expected %#v to be nil", req.Err)
+	}
 }
 
 func TestTwoRequestsOnePart(t *testing.T) {
-  buf, ch := setupPipe(
-    "*1\r\n$3\r\nfoo\r\n",
-    "*1\r\n$3\r\nbar\r\n",
-  )
-  go Scan(buf, ch);
+	buf, ch := setupPipe(
+		"*1\r\n$3\r\nfoo\r\n",
+		"*1\r\n$3\r\nbar\r\n",
+	)
+	go Scan(buf, ch)
 
-  var req *Request
-  req = <- ch
+	var req *Request
+	req = <-ch
 
-  assertEqual(t, 1, len(req.Parts), "")
-  assertEqual(t, "foo", string(req.Parts[0]), "")
-  if req.Err != nil {
-    t.Fatalf("expected %#v to be nil", req.Err)
-  }
+	assertEqual(t, 1, len(req.Parts), "")
+	assertEqual(t, "foo", string(req.Parts[0]), "")
+	if req.Err != nil {
+		t.Fatalf("expected %#v to be nil", req.Err)
+	}
 
-  req = <- ch
+	req = <-ch
 
-  assertEqual(t, 1, len(req.Parts), "")
-  assertEqual(t, "bar", string(req.Parts[0]), "")
-  if req.Err != nil { t.Fatalf("expected %#v to be nil", req.Err) }
+	assertEqual(t, 1, len(req.Parts), "")
+	assertEqual(t, "bar", string(req.Parts[0]), "")
+	if req.Err != nil {
+		t.Fatalf("expected %#v to be nil", req.Err)
+	}
 }
 
 func TestOneRequestTwoParts(t *testing.T) {
-  buf, ch := setupPipe("*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n")
-  go Scan(buf, ch);
+	buf, ch := setupPipe("*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n")
+	go Scan(buf, ch)
 
-  var req *Request
-  req = <- ch
+	var req *Request
+	req = <-ch
 
-  assertEqual(t, 2, len(req.Parts), "")
-  assertEqual(t, "foo", string(req.Parts[0]), "")
-  assertEqual(t, "bar", string(req.Parts[1]), "")
-  if req.Err != nil { t.Fatalf("expected %#v to be nil", req.Err) }
+	assertEqual(t, 2, len(req.Parts), "")
+	assertEqual(t, "foo", string(req.Parts[0]), "")
+	assertEqual(t, "bar", string(req.Parts[1]), "")
+	if req.Err != nil {
+		t.Fatalf("expected %#v to be nil", req.Err)
+	}
 }
 
 func TestScanNumber(t *testing.T) {
@@ -79,58 +85,60 @@ func TestScanNumber(t *testing.T) {
 }
 
 func TestOneRequestWithError(t *testing.T) {
-  testCases := []string{
-    "a",
-    "$a",
-    "$1a",
-    "$1",
-    "*a",
-    "*1a",
-    "*1\na",
-    "*1\n$a",
-    "*1\n$1a",
-    "a\n",
-    "$a\n",
-    "$1a\n",
-    "$1\n",
-    "*a\n",
-    "*1a\n",
-    "*1\na\n",
-    "*1\n$a\n",
-    "*1\n$1a\n",
-  }
+	testCases := []string{
+		"a",
+		"$a",
+		"$1a",
+		"$1",
+		"*a",
+		"*1a",
+		"*1\na",
+		"*1\n$a",
+		"*1\n$1a",
+		"a\n",
+		"$a\n",
+		"$1a\n",
+		"$1\n",
+		"*a\n",
+		"*1a\n",
+		"*1\na\n",
+		"*1\n$a\n",
+		"*1\n$1a\n",
+	}
 
-  for _, data := range(testCases) {
-    buf, ch := setupPipe(data)
-    go Scan(buf, ch);
+	for _, data := range testCases {
+		buf, ch := setupPipe(data)
+		go Scan(buf, ch)
 
-    var req *Request
-    req = <- ch
+		var req *Request
+		req = <-ch
 
-    if req.Err == nil {
-        t.Errorf("%q: expected an error", data)
-    }
-    if req.Parts != nil { t.Fatalf("expected %#v to be nil", req.Err) }
-  }
+		if req.Err == nil {
+			t.Errorf("%q: expected an error", data)
+		}
+		if req.Parts != nil {
+			t.Fatalf("expected %#v to be nil", req.Err)
+		}
+	}
 }
 
 func TestScanNumberWithError(t *testing.T) {
-  testCases := []string{
-    "a\n",
-    "$a\n",
-    "$1a\n",
-    "a",
-    "$a",
-    "$1a",
-  }
+	testCases := []string{
+		"a\n",
+		"$a\n",
+		"$1a\n",
+		"a",
+		"$a",
+		"$1a",
+	}
 
-  for _, data := range(testCases) {
-    buf, _ := setupPipe(data)
-    n, err := scanNumber(buf, '$')
+	for _, data := range testCases {
+		buf, _ := setupPipe(data)
+		n, err := scanNumber(buf, '$')
 
-    assertEqual(t, uint64(0), n, fmt.Sprintf("%q", data))
-    if err == nil {
-        t.Errorf("expected error for %q, got nil", data)
-    }
-  }
+		assertEqual(t, uint64(0), n, fmt.Sprintf("%q", data))
+		if err == nil {
+			t.Errorf("expected error for %q, got nil", data)
+		}
+	}
 }
