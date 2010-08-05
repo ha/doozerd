d3606e886648e94df5d2452707ce0929fc2882ba
Blake Mizerany <blake.mizerany@gmail.com>
2010-08-04 17:14:40 -0700

    gofmt

--- cmd/borgd/borgd.go
+++ cmd/borgd/borgd.go
@@ -10,26 +10,26 @@ import (
 var values = make(map[string][]byte)
 
 func main() {
-  listener, err := net.Listen("tcp", "0.0.0.0:9999")
-  if err != nil {
-    panic(err)
-  }
+	listener, err := net.Listen("tcp", "0.0.0.0:9999")
+	if err != nil {
+		panic(err)
+	}
 
-  ch := make(chan *proto.Request)
-  go func() {
-    //PAXOS!
-    for req := range(ch) {
-      fmt.Printf("Err:%v | Parts:%v\n", req.Err, req.Parts)
-    }
-  }()
+	ch := make(chan *proto.Request)
+	go func() {
+		//PAXOS!
+		for req := range ch {
+			fmt.Printf("Err:%v | Parts:%v\n", req.Err, req.Parts)
+		}
+	}()
 
-  for {
-    conn, err := listener.Accept()
-    if err != nil {
-      panic(err)
-    }
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
 
-    go proto.Scan(bufio.NewReader(conn), ch)
-  }
+		go proto.Scan(bufio.NewReader(conn), ch)
+	}
 
 }
