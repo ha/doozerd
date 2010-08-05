15b885ea6cf411ed2adde7238f280299d4f9106b
Blake Mizerany <blake.mizerany@gmail.com>
2010-08-04 17:38:07 -0700

    get and set work

--- cmd/borgd/borgd.go
+++ cmd/borgd/borgd.go
@@ -18,8 +18,32 @@ func main() {
 	ch := make(chan *proto.Request)
 	go func() {
 		//PAXOS!
+		var arity int
 		for req := range ch {
-			fmt.Printf("Err:%v | Parts:%v\n", req.Err, req.Parts)
+			if req.Err != nil {
+				fmt.Printf("Err:%v | Parts:%v\n", req.Err, req.Parts)
+				continue
+			}
+
+			arity = len(req.Parts) - 1
+
+			switch string(req.Parts[0]) {
+			case "set":
+				fmt.Printf("SET!\n")
+				if arity < 2 {
+					fmt.Printf("-ERR: %d for 2 arguments\n", arity)
+					break
+				}
+				values[string(req.Parts[1])] = req.Parts[2]
+			case "get":
+				fmt.Printf("GET!\n")
+				if arity < 1 {
+					fmt.Printf("-ERR: %d for 1 arguments\n", arity)
+					break
+				}
+				got := values[string(req.Parts[1])]
+				fmt.Printf("got: %v\n", got)
+			}
 		}
 	}()
 
