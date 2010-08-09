b05ee62ebf4c8f0a404647893fec99697b570feb
Keith Rarick <kr@xph.us>
2010-08-09 01:04:47 -0700

    router: started

--- pkg/router/router_test.go
+++ pkg/router/router_test.go
@@ -1,9 +1,51 @@
 package router
 
 import (
+    "borg/paxos"
+
+    "borg/assert"
     "testing"
 )
 
-func TestPass(t *testing.T) {
-    t.Fatal("boom")
+type cf func(me, nNodes uint64, target string, ins, outs chan paxos.Msg, clock chan int)
+type af func(me uint64, ins, outs chan paxos.Msg)
+type lf func(quorum int, ins chan paxos.Msg, taught chan string, ack func())
+
+func router(propIns, packetIns, packetOuts chan string, c cf, a af, l lf) {
+    target := <-propIns
+    go c(1, 2, target, nil, nil, nil)
+    go a(1, nil, nil)
+    go l(1, nil, nil, nil)
 }
+
+func TestStartNewPaxos(t *testing.T) {
+    val := "set:foo:bar"
+    cTarget := make(chan string)
+    aStarted := make(chan int)
+    lStarted := make(chan int)
+    propIns := make(chan string)
+
+    c := func(me, nNodes uint64, target string, ins, outs chan paxos.Msg, clock chan int) {
+        cTarget <- target
+    }
+
+    a := func(me uint64, ins, outs chan paxos.Msg) {
+        aStarted <- 1
+    }
+
+    l := func(quorum int, ins chan paxos.Msg, taught chan string, ack func()) {
+        lStarted <- 1
+    }
+
+    go router(propIns, nil, nil, c, a, l)
+
+    propIns <- val
+
+    // expect:
+    //  - new paxos id generated -- how to test?
+
+    assert.Equal(t, val, <-cTarget, "")
+    assert.Equal(t, 1, <-aStarted, "")
+    assert.Equal(t, 1, <-lStarted, "")
+}
+
