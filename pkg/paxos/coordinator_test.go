2793a3f94d6529c94736dba1ca250f85d18ae252
Blake Mizerany <blake.mizerany@gmail.com>
2010-08-08 18:45:49 -0700

    paxos: coord: start new round on timeout

--- pkg/paxos/coordinator_test.go
+++ pkg/paxos/coordinator_test.go
@@ -12,13 +12,14 @@ var (
 	IdOutOfRange = os.NewError("Id Out of Range")
 )
 
-func coordinator(me, nNodes uint64, v string, ins, outs chan msg) {
+func coordinator(me, nNodes uint64, v string, ins, outs chan msg, clock chan int) {
 	if me > nNodes {
 		panic(IdOutOfRange)
 	}
 
 	var crnd uint64 = me
 
+Start:
 	start := msg{
 		cmd:  "INVITE",
 		to:   0, // send to all acceptors
@@ -27,16 +28,29 @@ func coordinator(me, nNodes uint64, v string, ins, outs chan msg) {
 	}
 	outs <- start
 
-	for in := range ins {
-		switch in.cmd {
-		case "RSVP":
-			choosen := msg{
-				cmd:  "NOMINATE",
-				to:   0, // send to all acceptors
-				from: me,
-				body: fmt.Sprintf("%d:%s", crnd, v),
+	var rsvps uint64
+
+	quorum := nNodes/2 + 1
+
+	for {
+		select {
+		case in := <-ins:
+			switch in.cmd {
+			case "RSVP":
+				rsvps++
+				if rsvps >= quorum {
+					choosen := msg{
+						cmd:  "NOMINATE",
+						to:   0, // send to all acceptors
+						from: me,
+						body: fmt.Sprintf("%d:%s", crnd, v),
+					}
+					go func() { outs <- choosen }()
+				}
 			}
-			go func() { outs <- choosen }()
+		case <-clock:
+			crnd += nNodes
+			goto Start
 		}
 	}
 }
@@ -49,13 +63,14 @@ func coordinator(me, nNodes uint64, v string, ins, outs chan msg) {
 func TestStartsRoundAtMe(t *testing.T) {
 	ins := make(chan msg)
 	outs := make(chan msg)
+	clock := make(chan int)
 
 	nNodes := uint64(10) // this is arbitrary
 
 	res := make([]msg, 2)
-	go coordinator(1, nNodes, "foo", ins, outs)
+	go coordinator(1, nNodes, "foo", ins, outs, clock)
 	res[0] = <-outs
-	go coordinator(2, nNodes, "foo", ins, outs)
+	go coordinator(2, nNodes, "foo", ins, outs, clock)
 	res[1] = <-outs
 
 	exp := msgs("1:*:INVITE:1", "2:*:INVITE:2")
@@ -66,28 +81,32 @@ func TestStartsRoundAtMe(t *testing.T) {
 func TestPanicWhenMeIsOutOfRange(t *testing.T) {
 	ins := make(chan msg)
 	outs := make(chan msg)
+	clock := make(chan int)
 
 	nNodes := uint64(10) // this is arbitrary
 	assert.Panic(t, IdOutOfRange, func() {
-		coordinator(11, nNodes, "foo", ins, outs)
+		coordinator(11, nNodes, "foo", ins, outs, clock)
 	})
 }
 
-func TestPhase2aSimple(t *testing.T) {
+func TestPhase2aTimeoutStartsNewRound(t *testing.T) {
 	ins := make(chan msg)
 	outs := make(chan msg)
+	clock := make(chan int)
 
 	nNodes := uint64(10) // this is arbitrary
-	go coordinator(1, nNodes, "foo", ins, outs)
+	go coordinator(1, nNodes, "foo", ins, outs, clock)
 	<-outs //discard INVITE
 
+	// never reach majority (force timeout)
 	ins <- m("2:1:RSVP:1:0:")
 	ins <- m("3:1:RSVP:1:0:")
 	ins <- m("4:1:RSVP:1:0:")
 	ins <- m("5:1:RSVP:1:0:")
 	ins <- m("6:1:RSVP:1:0:")
-	ins <- m("7:1:RSVP:1:0:")
 
-	exp := m("1:*:NOMINATE:1:foo")
+	clock <- 1
+
+	exp := m("1:*:INVITE:11")
 	assert.Equal(t, exp, <-outs, "")
 }
