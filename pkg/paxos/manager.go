c88ea8a7b73da0096e82a025e8c3de7eef4de9ab
Keith Rarick <kr@xph.us>
2010-08-13 13:04:21 -0700

    split out manager impl

---
+++ pkg/paxos/manager.go
@@ -0,0 +1,73 @@
+package paxos
+
+type Result struct {
+	seqn uint64
+	v string
+}
+
+type instReq struct {
+	seqn uint64
+	ch chan *Instance
+}
+
+type Manager struct{
+	learned chan Result
+	reqs chan instReq
+	seqns chan uint64
+}
+
+func NewManager() *Manager {
+	m := &Manager{
+		learned: make(chan Result),
+		reqs: make(chan instReq),
+		seqns: make(chan uint64),
+	}
+	return m
+}
+
+func (m *Manager) Init(outs Putter) {
+	go func() {
+		instances := make(map[uint64]*Instance)
+		for req := range m.reqs {
+			inst, ok := instances[req.seqn]
+			if !ok {
+				inst = NewInstance(1, 1)
+				inst.Init(PutWrapper{req.seqn, 1, outs})
+				instances[req.seqn] = inst
+				go func() {
+					m.learned <- Result{req.seqn, inst.Value()}
+				}()
+			}
+			req.ch <- inst
+		}
+	}()
+
+	// Generate an infinite stream of sequence numbers (seqns).
+	go func() {
+		for n := uint64(1); ; n++ {
+			m.seqns <- n
+		}
+	}()
+}
+
+func (m *Manager) getInstance(seqn uint64) *Instance {
+	ch := make(chan *Instance)
+	m.reqs <- instReq{seqn, ch}
+	return <-ch
+}
+
+func (m *Manager) Put(msg Msg) {
+	m.getInstance(msg.seqn).Put(msg)
+}
+
+func (m *Manager) Propose(v string) string {
+	inst := m.getInstance(<-m.seqns)
+	inst.Propose(v)
+	return inst.Value()
+}
+
+func (m *Manager) Recv() (uint64, string) {
+	result := <-m.learned
+	return result.seqn, result.v
+}
+
