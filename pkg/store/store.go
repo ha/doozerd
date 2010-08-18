656f91bb9b26b4fda232014adafde2f852851735
Keith Rarick <kr@xph.us>
2010-08-18 03:37:17 -0700

    Working storage.

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -1,5 +1,12 @@
 package store
 
+import (
+	"container/vector"
+	"container/heap"
+	"os"
+	"strings"
+)
+
 type Event struct {
 	Type int
 	Path string
@@ -13,15 +20,111 @@ const (
 	Rem
 )
 
-func Encode(path, value string) (mutation string)
+var (
+	BadPathError = os.NewError("bad path")
+	BadMutationError = os.NewError("bad mutation")
+)
 
-func decode(mutation string) (path, value string)
+type Store struct {
+	applyCh chan apply
+	reqCh chan req
+}
 
-func Apply(seqn uint64, mutation string)
+type apply struct {
+	seqn uint64
+	k string
+	v string
+}
+
+type req struct {
+	k string
+	ch chan reply
+}
+
+type reply struct {
+	v string
+	ok bool
+}
+
+type applyHeap struct {
+	vector.Vector
+}
+
+func (ah applyHeap) Less(i, j int) bool {
+	return ah.Seqn(i) < ah.Seqn(j)
+}
+
+func (ah applyHeap) Seqn(i int) uint64 {
+	return ah.At(i).(*apply).seqn
+}
+
+func NewStore() *Store {
+	next := uint64(1)
+	todo := new(applyHeap)
+	heap.Init(todo)
+	values := make(map[string]string)
+	reqCh := make(chan req)
+	applyCh := make(chan apply)
+	go func() {
+		for {
+			select {
+			case a := <-applyCh:
+				heap.Push(todo, &a)
+				for todo.Len() > 0 && todo.Seqn(0) == next {
+					t := heap.Pop(todo).(*apply)
+					values[t.k] = t.v
+					next++
+				}
+			case r := <-reqCh:
+				v, ok := values[r.k]
+				r.ch <- reply{v, ok}
+			}
+		}
+	}()
+	return &Store{
+		applyCh: applyCh,
+		reqCh: reqCh,
+	}
+}
+
+func Encode(path, v string) (mutation string, err os.Error) {
+	switch {
+	case len(path) < 1,
+	     path[0] != '/',
+	     strings.Count(path, "=") > 0,
+	     strings.Count(path, " ") > 0:
+		return "", BadPathError
+	}
+	return path + "=" + v, nil
+}
+
+func decode(mutation string) (path, v string, err os.Error) {
+	parts := strings.Split(mutation, "=", 2)
+	if len(parts) < 2 {
+		return "", "", BadMutationError
+	}
+	return parts[0], parts[1], nil
+}
+
+func (s *Store) Apply(seqn uint64, mutation string) {
+	path, v, err := decode(mutation)
+	if err != nil {
+		return
+	}
+	s.applyCh <- apply{seqn, path, v}
+}
 
 // For a missing path, `ok == false`. Otherwise, it is `true`.
-func Lookup(path string) (value string, ok bool)
+func (s *Store) Lookup(path string) (v string, ok bool) {
+	ch := make(chan reply)
+	s.reqCh <- req{path, ch}
+	rep := <-ch
+	//v, ok = s.values[path]
+	return rep.v, rep.ok
+}
 
 // `eventMask` is one or more of `Set`, `Del`, `Add`, and `Rem`, bitwise OR-ed
 // together.
-func Watch(path string, eventMask byte) (events chan Event)
+func (s *Store) Watch(path string, eventMask byte) (events chan Event) {
+	return make(chan Event)
+}
---
+++ pkg/store/store_test.go
@@ -0,0 +1,109 @@
+package store
+
+import (
+	"borg/assert"
+	"testing"
+)
+
+var KVMs = [][3]string{
+	[3]string{"/", "a", "/=a"},
+	[3]string{"/x", "a", "/x=a"},
+	[3]string{"/x", "a=b", "/x=a=b"},
+	[3]string{"/x", "a b", "/x=a b"},
+}
+
+var BadPaths = []string{
+	"",
+	"x",
+	"/x=",
+	"/x y",
+}
+
+var BadMutations = []string{
+	"x",
+}
+
+func TestEncode(t *testing.T) {
+	for _, kvm := range KVMs {
+		k, v, exp := kvm[0], kvm[1], kvm[2]
+		got, err := Encode(k, v)
+		if err != nil {
+			t.Error("unexpected error:", err)
+		}
+		assert.Equal(t, exp, got, "")
+	}
+}
+
+func TestEncodeBadPaths(t *testing.T) {
+	for _, k := range BadPaths {
+		_, err := Encode(k, "")
+		if err != BadPathError {
+			t.Errorf("expected BadPathError on %q", k)
+		}
+	}
+}
+
+func TestDecode(t *testing.T) {
+	for _, kvm := range KVMs {
+		expk, expv, m := kvm[0], kvm[1], kvm[2]
+		gotk, gotv, err := decode(m)
+		if err != nil {
+			t.Error(err)
+		}
+		assert.Equal(t, expk, gotk, "key from " + m)
+		assert.Equal(t, expv, gotv, "value from " + m)
+	}
+}
+
+func TestDecodeBadMutations(t *testing.T) {
+	for _, m := range BadMutations {
+		_, _, err := decode(m)
+		if err != BadMutationError {
+			t.Errorf("expected BadMutationError on %q", m)
+		}
+	}
+}
+
+func TestApply(t *testing.T) {
+	s := NewStore()
+	mut, _ := Encode("/x", "a")
+	s.Apply(1, mut)
+}
+
+func TestLookupMissing(t *testing.T) {
+	s := NewStore()
+	v, ok := s.Lookup("/x")
+	assert.Equal(t, false, ok, "")
+	assert.Equal(t, "", v, "")
+}
+
+func TestLookup(t *testing.T) {
+	s := NewStore()
+	mut, _ := Encode("/x", "a")
+	s.Apply(1, mut)
+	v, ok := s.Lookup("/x")
+	assert.Equal(t, true, ok, "")
+	assert.Equal(t, "a", v, "")
+}
+
+func TestApplyInOrder(t *testing.T) {
+	s := NewStore()
+	mut1, _ := Encode("/x", "a")
+	mut2, _ := Encode("/x", "b")
+	s.Apply(1, mut1)
+	s.Apply(2, mut2)
+	v, ok := s.Lookup("/x")
+	assert.Equal(t, true, ok, "")
+	assert.Equal(t, "b", v, "")
+}
+
+func TestApplyOutOfOrder(t *testing.T) {
+	s := NewStore()
+	mut1, _ := Encode("/x", "a")
+	mut2, _ := Encode("/x", "b")
+	s.Apply(2, mut2)
+	s.Apply(1, mut1)
+	v, ok := s.Lookup("/x")
+	assert.Equal(t, true, ok, "")
+	assert.Equal(t, "b", v, "")
+}
