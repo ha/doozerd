87c2cefbacf0f67e7950aae1b9bbaa44cd7c5d67
Blake Mizerany <blake.mizerany@gmail.com>
2010-08-19 18:37:08 -0700

    simple client finished

---
+++ pkg/client/client.go
@@ -0,0 +1,39 @@
+package client
+
+import (
+	"io"
+	"os"
+	"borg/proto"
+)
+
+type Client struct {
+	r proto.ReadStringer
+	w io.Writer
+}
+
+// Errors that can happen
+var (
+	InvalidResponse = os.NewError("Invalid Response from server.")
+)
+
+func (c *Client) Set(path, body string) os.Error {
+	err := proto.Encode(c.w, "set", path, body)
+	if err != nil {
+		return err
+	}
+	_, err = proto.Decode(c.r)
+	return err
+}
+
+func (c *Client) Get(path string) (string, os.Error) {
+	err := proto.Encode(c.w, "get", path)
+	if err != nil {
+		return "", err
+	}
+	parts, err := proto.Decode(c.r)
+	if len(parts) > 0 {
+		return parts[0], err
+	}
+	return "", err
+}
+
---
+++ pkg/client/client_test.go
@@ -0,0 +1,69 @@
+package client
+
+import (
+	"testing"
+	"bytes"
+	"bufio"
+	"os"
+
+	"borg/assert"
+	"borg/proto"
+)
+
+// TESTING
+//
+// TODO:  Better tests please.  As of the time of writing,
+// we're still on the fence about the API, so we're only
+// testing low-hanging fruit.
+
+func TestClientSet(t *testing.T) {
+	resp := new(bytes.Buffer)
+	proto.Encode(resp, "123")
+
+	r := bufio.NewReader(resp)
+	w := bufio.NewWriter(new(bytes.Buffer))
+	client := &Client{r, w}
+
+	err := client.Set("foo", "bar")
+
+	assert.True(t, err == nil)
+}
+
+func TestClientSetWithEOF(t *testing.T) {
+	resp := bytes.NewBufferString("*1\rASDF")
+
+	r := bufio.NewReader(resp)
+	w := bufio.NewWriter(new(bytes.Buffer))
+	client := &Client{r, w}
+
+	err := client.Set("foo", "bar")
+
+	assert.Equal(t, os.EOF, err, "")
+}
+
+func TestClientGetSimple(t *testing.T) {
+	resp := new(bytes.Buffer)
+	proto.Encode(resp, "bar")
+
+	r := bufio.NewReader(resp)
+	w := bufio.NewWriter(new(bytes.Buffer))
+	client := &Client{r, w}
+
+	body, err := client.Get("foo")
+
+	assert.True(t, err == nil)
+	assert.Equal(t, "bar", body, "")
+}
+
+func TestClientGetWithEOF(t *testing.T) {
+	resp := bytes.NewBufferString("*1\rASDF")
+
+	r := bufio.NewReader(resp)
+	w := bufio.NewWriter(new(bytes.Buffer))
+	client := &Client{r, w}
+
+	body, err := client.Get("foo")
+
+	assert.Equal(t, os.EOF, err, "")
+	assert.Equal(t, "", body, "")
+}
