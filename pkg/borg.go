8bccfa6fc059deb62510bd193c8bb6258d4b299b
Keith Rarick <kr@xph.us>
2010-08-19 18:34:54 -0700

    move this doc to the top

--- pkg/borg.go
+++ pkg/borg.go
@@ -1,5 +1,47 @@
 package borg
 
+// Think of borg events like inotify events.  We're interested in changes to
+// them all.  All events are sent to your instance of borg.  The filtering
+// is done once they've arrived.  Let's make keys look like directories to
+// aid this comparison.
+//
+// Example spec:
+//
+//     /<slug_id>/proc/<type>/<upid>/...
+//
+// Example interactive session:
+//
+//     $ borgd -i -a :9999
+//     >> ls /proc/123_a3c_a12b3c45/beanstalkd/12345/
+//     exe
+//     env
+//     lock
+//     >> cat /proc/123_a3c_a12b3c45/beanstalkd/12345/*
+//     beanstalkd -l 0.0.0.0 -p 4563
+//     PORT=4563
+//     123.4.5.678:9999
+//     >>
+//
+// Example code:
+//
+//     me, err := borg.ListenAndServe(listenAddr)
+//     if err != nil {
+//         log.Exitf("listen failed: %v", err)
+//     }
+//
+//     // Handle a specific type of key notification.
+//     // The : signals a named variable part.
+//     me.HandleFunc(
+//         "/proc/:slug/beanstalkd/:upid/lock",
+//         func (msg *borg.Message) {
+//             if msg.Value == myId {
+//                 cmd := beanstalkd ....
+//                 ... launch beanstalkd ...
+//                 me.Echo(cmd, "/proc/<slug>/beanstalkd/<upid>/cmd")
+//             }
+//         },
+//     )
+
 import (
 	"borg/paxos"
 	"borg/store"
@@ -209,46 +251,4 @@ func (n *Node) RunForever() {
 		fmt.Printf("learned %d %#v\n", seqn, v)
 		//store.Apply(manager.Recv())
 	}
-
-	// Think of borg events like inotify events.  We're interested in changes to
-	// them all.  All events are sent to your instance of borg.  The filtering
-	// is done once they've arrived.  Let's make keys look like directories to
-	// aid this comparison.
-	//
-	// Example spec:
-	//
-	//     /<slug_id>/proc/<type>/<upid>/...
-	//
-	// Example interactive session:
-	//
-	//     $ borgd -i -a :9999
-	//     >> ls /proc/123_a3c_a12b3c45/beanstalkd/12345/
-	//     exe
-	//     env
-	//     lock
-	//     >> cat /proc/123_a3c_a12b3c45/beanstalkd/12345/*
-	//     beanstalkd -l 0.0.0.0 -p 4563
-	//     PORT=4563
-	//     123.4.5.678:9999
-	//     >>
-	//
-	// Example code:
-	//
-	//     me, err := borg.ListenAndServe(listenAddr)
-	//     if err != nil {
-	//         log.Exitf("listen failed: %v", err)
-	//     }
-	//
-	//     // Handle a specific type of key notification.
-	//     // The : signals a named variable part.
-	//     me.HandleFunc(
-	//         "/proc/:slug/beanstalkd/:upid/lock",
-	//         func (msg *borg.Message) {
-	//             if msg.Value == myId {
-	//                 cmd := beanstalkd ....
-	//                 ... launch beanstalkd ...
-	//                 me.Echo(cmd, "/proc/<slug>/beanstalkd/<upid>/cmd")
-	//             }
-	//         },
-	//     )
 }
