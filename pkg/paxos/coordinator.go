5f7c502c60629180979360f228caafc796377b52
Keith Rarick <kr@xph.us>
2010-08-24 01:55:29 -0700

    refactor

--- pkg/paxos/coordinator.go
+++ pkg/paxos/coordinator.go
@@ -46,7 +46,6 @@ func (f FakeCluster) Quorum() int {
 // TODO temporary name
 type C struct {
 	cluster Cluster
-	quorum uint64
 	modulus uint64
 
 	ins chan Msg
@@ -70,7 +69,6 @@ func coordinator(crnd, quorum, modulus uint64, tCh chan string, ins chan Msg, ou
 func NewC(c Cluster) *C {
 	return &C{
 		cluster: c,
-		quorum: uint64(c.Quorum()),
 		modulus: uint64(c.Len()),
 		outs: c,
 	}
