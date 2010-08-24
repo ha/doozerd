0a1a1b4ca8c4b31676d1d78d14aa1140646668f1
Keith Rarick <kr@xph.us>
2010-08-23 18:05:43 -0700

    another test

--- pkg/store/store_test.go
+++ pkg/store/store_test.go
@@ -223,6 +223,44 @@ func TestLookupSyncSeveral(t *testing.T) {
 	assert.Equal(t, true, <-chok, "")
 }
 
+func TestLookupSyncExtra(t *testing.T) {
+	chv := make(chan string)
+	chok := make(chan bool)
+	s := New(logger)
+	mut1, _ := EncodeSet("/x", "a")
+	mut2, _ := EncodeSet("/x", "b")
+	mut3, _ := EncodeSet("/x", "c")
+	go func() {
+		v, ok := s.LookupSync("/x", 0)
+		chv <- v
+		chok <- ok
+
+		v, ok = s.LookupSync("/x", 5)
+		chv <- v
+		chok <- ok
+
+		v, ok = s.LookupSync("/x", 0)
+		chv <- v
+		chok <- ok
+	}()
+	s.Apply(1, mut1)
+	s.Apply(2, mut1)
+	s.Apply(3, mut1)
+	s.Apply(4, mut1)
+
+	s.Apply(6, mut3)
+	s.Apply(7, mut3)
+	s.Apply(8, mut3)
+
+	s.Apply(5, mut2)
+	assert.Equal(t, "a", <-chv, "")
+	assert.Equal(t, true, <-chok, "")
+	assert.Equal(t, "c", <-chv, "")
+	assert.Equal(t, true, <-chok, "")
+	assert.Equal(t, "c", <-chv, "")
+	assert.Equal(t, true, <-chok, "")
+}
+
 func TestApplyBadThenGood(t *testing.T) {
 	s := New(logger)
 	mut1 := "foo" // bad mutation
