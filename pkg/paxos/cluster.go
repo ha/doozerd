ac4f60254cfe8d325456f74b15491a9b6e9bc16a
Keith Rarick <kr@xph.us>
2010-08-24 17:15:38 -0700

    start cluster type

---
+++ pkg/paxos/cluster.go
@@ -0,0 +1,50 @@
+package paxos
+
+import (
+    "sort"
+)
+
+type Node struct {
+    id string
+}
+
+// TODO this is a bad name. I predict that we should soon get rid of the
+// "Cluster" *interface* and rename this type to "Cluster".
+type RealCluster struct {
+    self string
+    nodes []string
+    fw Putter
+    selfIndex int
+}
+
+func NewCluster(self string, nodes []string, fw Putter) Cluster {
+    sort.SortStrings(nodes)
+    selfIndex := -1
+    for i, id := range nodes {
+        if id == self {
+            selfIndex = i
+        }
+    }
+    return &RealCluster{
+        self: self,
+        nodes: nodes,
+        fw: fw,
+        selfIndex: selfIndex,
+    }
+}
+
+func (cx *RealCluster) Len() int {
+    return len(cx.nodes)
+}
+
+func (cx *RealCluster) Quorum() int {
+    return cx.Len()/2 + 1
+}
+
+func (cx *RealCluster) SelfIndex() int {
+    return cx.selfIndex
+}
+
+func (cx *RealCluster) Put(m Msg) {
+    cx.fw.Put(m)
+}
---
+++ pkg/paxos/cluster_test.go
@@ -0,0 +1,17 @@
+package paxos
+
+import (
+    "borg/assert"
+    "testing"
+)
+
+func TestCluster(t *testing.T) {
+    outs := make(chan Msg)
+    cx := NewCluster("c", []string{"a", "b", "c"}, ChanPutter(outs))
+    assert.Equal(t, 3, cx.Len(), "Len")
+    assert.Equal(t, 2, cx.Quorum(), "Quorum")
+    assert.Equal(t, 2, cx.SelfIndex(), "SelfIndex")
+    msg := m("1:1:INVITE:1")
+    cx.Put(msg)
+    assert.Equal(t, msg, <-outs, "Put")
+}
