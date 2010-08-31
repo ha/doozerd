ea1c863e010fcd2e5afb1810bb55f5abcaf9bcbe
Blake Mizerany <blake.mizerany@gmail.com>
2010-08-31 13:52:53 -0700

    wipe out pkg/junta.go and cmd/juntad/juntad.go

--- cmd/juntad/juntad.go
+++ cmd/juntad/juntad.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"junta"
-	"junta/store"
 	"flag"
 	"log"
 	"os"
-	"strings"
 )
 
 // Flags
@@ -21,61 +18,6 @@ var (
 	logger *log.Logger = log.New(os.Stderr, nil, "juntad: ", log.Lok)
 )
 
-func acquire(path string) {
-	// TODO acquire a lock
-	// N.B. this should block
-}
-
-func release(path string) {
-	// TODO release lock
-}
-
-func run(s *store.Store, path string) {
-	for {
-		acquire(path)
-		cmd, ok := s.Lookup(path)
-		if !ok {
-			return
-		}
-
-		// TODO better tokenization (e.g. quoted strings, variables)
-		args := strings.Split(cmd, " ", -1)
-		logger.Logf("starting %s", cmd)
-		pid, err := os.ForkExec(args[0], args, nil, "/", nil)
-		if err != nil {
-			logger.Logf("fatal: %v", err)
-			return
-		}
-		logger.Logf("started process %d", pid)
-		w, err := os.Wait(pid, 0)
-		if err != nil {
-			logger.Logf("wtf: %v", err)
-			return
-		}
-		logger.Logf("process %d exited with %v", pid, w)
-		release(path)
-	}
-}
-
 func main() {
 	flag.Parse()
-	s := store.New(logger)
-	b := junta.New(*id, *listenAddr, s, logger)
-
-	adds := make(chan store.Event)
-	s.Watch("/service", store.Add, adds)
-	go func() {
-		for ev := range adds {
-			path := ev.Path + "/" + ev.Body
-			logger.Logf("oh, %s has appeared", path)
-			go run(s, path)
-		}
-	}()
-
-	if *attachAddr == "" {
-		b.Init()
-	} else {
-		b.Join(*attachAddr)
-	}
-	b.RunForever()
 }
