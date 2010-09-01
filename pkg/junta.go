9da69affce8e22dc2fccf2c58ccaee1fc71e3883
Blake Mizerany <blake.mizerany@gmail.com>
2010-09-01 01:16:49 -0700

    be more like http pkg

--- pkg/junta.go
+++ pkg/junta.go
@@ -9,28 +9,35 @@ import (
 	"junta/proto"
 )
 
+type conn struct {
+	net.Conn
+}
+
 type server struct {
 	net.Listener
 	logger *log.Logger
 }
 
-func (s server) acceptAndServe() {
+func Serve(l net.Listener) os.Error {
 	for {
-		conn, err := s.Accept()
-		if err != nil {
-			s.logger.Logf("%s: %s", s.Listener, err)
-			continue
+		rw, e := l.Accept()
+		if e != nil {
+			//s.logger.Logf("%s: %s", s.Listener, err)
+			return e
 		}
-		go s.serve(conn)
+		c := &conn{rw}
+		go c.serve()
 	}
+
+	panic("not reached")
 }
 
-func (server) serve(conn net.Conn) {
-	c := proto.NewConn(conn)
-	logger := util.NewLogger("%v", conn.RemoteAddr())
+func (c *conn) serve() {
+	pc := proto.NewConn(c)
+	logger := util.NewLogger("%v", c.RemoteAddr())
 	logger.Log("accepted connection")
 	for {
-		rid, parts, err := c.ReadRequest()
+		rid, parts, err := pc.ReadRequest()
 		if err != nil {
 			if err == os.EOF {
 				logger.Log("connection closed by peer")
@@ -40,13 +47,13 @@ func (server) serve(conn net.Conn) {
 			return
 		}
 
-		rlogger := util.NewLogger("%v - req [%d]", conn.RemoteAddr(), rid)
+		rlogger := util.NewLogger("%v - req [%d]", c.RemoteAddr(), rid)
 		rlogger.Logf("received <%v>", parts)
 
 		if len(parts) == 0 {
 			rlogger.Log("len(parts) == 0")
 			rlogger.Log("before error")
-			c.SendError(rid, proto.InvalidCommand)
+			pc.SendError(rid, proto.InvalidCommand)
 			rlogger.Log("after error")
 			continue
 		}
@@ -54,11 +61,11 @@ func (server) serve(conn net.Conn) {
 		switch parts[0] {
 		default:
 			rlogger.Logf("unknown command <%s>", parts[0])
-			c.SendError(rid, proto.InvalidCommand)
+			pc.SendError(rid, proto.InvalidCommand)
 		case "set":
 			//go set(c, rid, parts[1:])
 			rlogger.Logf("set %q=%q", parts[1], parts[2])
-			c.SendResponse(rid, "OK")
+			pc.SendResponse(rid, "OK")
 		}
 		rlogger.Log("bottom")
 	}
@@ -68,16 +75,14 @@ func ListenAndServe(addr string) os.Error {
 	logger := util.NewLogger("server %s", addr)
 
 	logger.Logf("binding to %s", addr)
-	lisn, err := net.Listen("tcp", addr)
+	l, err := net.Listen("tcp", addr)
 	if err != nil {
 		logger.Log(err)
 		return err
 	}
-
 	logger.Logf("listening on %s", addr)
 
-	server := &server{lisn, logger}
-	server.acceptAndServe()
-
-	return nil
+	err = Serve(l)
+	l.Close()
+	return err
 }
