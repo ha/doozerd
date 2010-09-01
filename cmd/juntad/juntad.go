3195404f4b9ba7e46fe76552e69e7432ca74e896
Blake Mizerany <blake.mizerany@gmail.com>
2010-08-31 19:30:23 -0700

    the start of juntad

--- cmd/juntad/juntad.go
+++ cmd/juntad/juntad.go
@@ -4,6 +4,10 @@ import (
 	"flag"
 	"log"
 	"os"
+	"net"
+	"fmt"
+
+	"junta/proto"
 )
 
 // Flags
@@ -15,9 +19,39 @@ var (
 
 // Globals
 var (
-	logger *log.Logger = log.New(os.Stderr, nil, "juntad: ", log.Lok)
+	logger *log.Logger = log.New(
+		os.Stderr, nil,
+		"juntad: ",
+		log.Lok | log.Lshortfile,
+	)
 )
 
 func main() {
 	flag.Parse()
+
+	logger.Logf("binding to %s", *listenAddr)
+	lisn, err := net.Listen("tcp", *listenAddr)
+	if err != nil {
+		logger.Log("unable to listen on %s: %s", *listenAddr, err)
+		os.Exit(1)
+	}
+	logger.Logf("listening on %s", *listenAddr)
+
+	for {
+		conn, err := lisn.Accept()
+		if err != nil {
+			logger.Log("unable to accept on %s: %s", *listenAddr, err)
+			continue
+		}
+
+		go func() {
+			c := proto.NewConn(conn)
+			_, parts, err := c.ReadRequest()
+			if err != nil {
+				logger.Log(err)
+				return
+			}
+			fmt.Println(parts)
+		}()
+	}
 }
