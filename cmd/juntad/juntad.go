fd52784b5e55cd7c4f05c907f348ab8a0d7d0329
Keith Rarick <kr@xph.us>
2010-09-01 02:47:49 -0700

    up and running

--- cmd/juntad/juntad.go
+++ cmd/juntad/juntad.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"junta"
+	"junta/paxos"
+	"junta/store"
+	"junta/util"
 )
+
+const alpha = 50
 
 // Flags
 var (
@@ -13,5 +19,40 @@ var (
 
 func main() {
 	flag.Parse()
-	junta.ListenAndServe(*listenAddr)
+	store.Logger = util.NewLogger("store")
+	outs := make(chan paxos.Msg)
+	self := randString(160)
+
+	st := store.New()
+	rg := paxos.NewRegistrar(self, st, alpha)
+
+	addMember(st, self, *listenAddr)
+
+	mg := paxos.NewManager(2, rg, paxos.ChanPutCloser(outs))
+	go func() {
+		panic(junta.ListenAndServe(*listenAddr, st, mg))
+	}()
+
+	go func() {
+		panic(junta.ListenAndServeUdp(*listenAddr, mg, outs))
+	}()
+
+	for {
+		st.Apply(mg.Recv())
+	}
+}
+
+func addMember(st *store.Store, self, addr string) {
+	// TODO pull out path as a const
+	mx, err := store.EncodeSet("/j/junta/members/"+self, addr)
+	if err != nil {
+		panic(err)
+	}
+	st.Apply(1, mx)
+}
+
+func randString(bits int) string {
+	selfBytes := make([]byte, bits/8)
+	util.RandBytes(selfBytes)
+	return fmt.Sprintf("%x", selfBytes)
 }
--- pkg/junta.go
+++ pkg/junta.go
@@ -5,19 +5,62 @@ import (
 	"net"
 	
 	"junta/util"
+	"junta/paxos"
 	"junta/proto"
+	"junta/store"
 )
+
+const window = 50
 
 type conn struct {
 	net.Conn
+	s *server
 }
 
 type server struct {
 	net.Listener
+	mg *paxos.Manager
 }
 
-func Serve(l net.Listener) os.Error {
-	return (&server{l}).Serve()
+func Serve(l net.Listener, mg *paxos.Manager) os.Error {
+	return (&server{l, mg}).Serve()
+}
+
+func ServeUdp(u net.PacketConn, mg *paxos.Manager, outs chan paxos.Msg) os.Error {
+	logger := util.NewLogger("udp server %s", u.LocalAddr())
+	go func() {
+		logger.Log("reading messages...")
+		for {
+			// TODO pull out this magic number into a const
+			msg, addr, err := paxos.ReadMsg(u, 3000)
+			if err != nil {
+				logger.Log(err)
+				continue
+			}
+			logger.Logf("read %v from %s", msg, addr)
+			mg.PutFrom(addr, msg)
+		}
+	}()
+
+	logger.Log("sending messages...")
+	for msg := range outs {
+		logger.Logf("sending %v", msg)
+		for _, addr := range mg.AddrsFor(msg) {
+			logger.Logf("sending to %s", addr)
+			udpAddr, err := net.ResolveUDPAddr(addr)
+			if err != nil {
+				logger.Log(err)
+				continue
+			}
+			_, err = u.WriteTo(msg.WireBytes(), udpAddr)
+			if err != nil {
+				logger.Log(err)
+				continue
+			}
+		}
+	}
+
+	panic("not reached")
 }
 
 func (s *server) Serve() os.Error {
@@ -26,7 +69,7 @@ func (s *server) Serve() os.Error {
 		if e != nil {
 			return e
 		}
-		c := &conn{rw}
+		c := &conn{rw, s}
 		go c.serve()
 	}
 
@@ -64,26 +107,59 @@ func (c *conn) serve() {
 		case "set":
 			//go set(c, rid, parts[1:])
 			rlogger.Logf("set %q=%q", parts[1], parts[2])
-			pc.SendResponse(rid, "OK")
+			mut, err := store.EncodeSet(parts[1], parts[2])
+			if err != nil {
+				rlogger.Log(err)
+				pc.SendError(rid, err.String())
+			} else {
+				rlogger.Logf("propose %q", mut)
+				v := c.s.mg.Propose(mut)
+				if v == mut {
+					rlogger.Logf("good")
+					pc.SendResponse(rid, "true")
+				} else {
+					rlogger.Logf("bad")
+					pc.SendError(rid, "false")
+				}
+			}
 		}
 	}
 }
 
-func ListenAndServe(addr string) os.Error {
+func ListenAndServe(addr string, st *store.Store, mg *paxos.Manager) os.Error {
 	logger := util.NewLogger("server %s", addr)
 
-	logger.Logf("binding to %s", addr)
+	logger.Log("binding")
 	l, err := net.Listen("tcp", addr)
 	if err != nil {
 		logger.Log(err)
 		return err
 	}
 	defer l.Close()
-	logger.Logf("listening on %s", addr)
+	logger.Log("listening")
 
-	err = Serve(l)
+	err = Serve(l, mg)
 	if err != nil {
 		logger.Logf("%s: %s", l, err)
+	}
+	return err
+}
+
+func ListenAndServeUdp(addr string, mg *paxos.Manager, outs chan paxos.Msg) os.Error {
+	logger := util.NewLogger("udp server %s", addr)
+
+	logger.Log("binding")
+	u, err := net.ListenPacket("udp", addr)
+	if err != nil {
+		logger.Log(err)
+		return err
+	}
+	defer u.Close()
+	logger.Log("listening")
+
+	err = ServeUdp(u, mg, outs)
+	if err != nil {
+		logger.Logf("%s: %s", u, err)
 	}
 	return err
 }
