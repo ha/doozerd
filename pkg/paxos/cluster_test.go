95deb702fc6ef924ee838670ee161deda87e7b3c
Keith Rarick <kr@xph.us>
2010-09-09 18:03:53 -0700

    check empty cluster behavior

--- pkg/paxos/cluster_test.go
+++ pkg/paxos/cluster_test.go
@@ -44,3 +44,23 @@ func TestClusterActive(t *testing.T) {
 	assert.Equal(t, []string{"x", "y", "z"}, cx.addrs(), "addrs")
 	assert.Equal(t, membership, cx.addrsById)
 }
+
+func TestClusterEmpty(t *testing.T) {
+	membership := map[string]string{}
+	active := []string{}
+	cx := newCluster("a", membership, active)
+	assert.Equal(t, 0, cx.Len(), "Len")
+	assert.Equal(t, 1, cx.Quorum(), "Quorum")
+	assert.Equal(t, -1, cx.SelfIndex(), "SelfIndex")
+	assert.Equal(t, -1, cx.indexById("a"), "indexById(\"a\")")
+	assert.Equal(t, -1, cx.indexById("b"), "indexById(\"a\")")
+	assert.Equal(t, -1, cx.indexById("c"), "indexById(\"a\")")
+	assert.Equal(t, "", cx.idByAddr("x"), "idByAddr(\"a\")")
+	assert.Equal(t, "", cx.idByAddr("y"), "idByAddr(\"a\")")
+	assert.Equal(t, "", cx.idByAddr("z"), "idByAddr(\"a\")")
+	assert.Equal(t, -1, cx.indexByAddr("x"), "indexByAddr(\"a\")")
+	assert.Equal(t, -1, cx.indexByAddr("y"), "indexByAddr(\"a\")")
+	assert.Equal(t, -1, cx.indexByAddr("z"), "indexByAddr(\"a\")")
+	assert.Equal(t, []string{}, cx.addrs(), "addrs")
+	assert.Equal(t, membership, cx.addrsById)
+}
