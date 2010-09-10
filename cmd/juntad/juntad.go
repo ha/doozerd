091da88fa483025bfc907802451d927003348692
Keith Rarick <kr@xph.us>
2010-09-09 18:55:14 -0700

    default listen addr

--- cmd/juntad/juntad.go
+++ cmd/juntad/juntad.go
@@ -18,7 +18,7 @@ const (
 
 // Flags
 var (
-	listenAddr *string = flag.String("l", ":8040", "The address to bind to.")
+	listenAddr *string = flag.String("l", "[::1]:8040", "The address to bind to.")
 	attachAddr *string = flag.String("a", "", "The address to bind to.")
 )
 
