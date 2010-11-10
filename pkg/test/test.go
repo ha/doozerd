4088a506c13fe2aa3fd345aa76e381967124dbc6
Blake Mizerany <blake.mizerany@gmail.com>
2010-11-09 18:02:08 -0800

    test: add ErrWriter

--- pkg/test/test.go
+++ pkg/test/test.go
@@ -17,3 +17,17 @@ func (fp *FakeProposer) Propose(v string) (uint64, string, os.Error) {
 	ev := <-ch
 	return fp.seqn, ev.Cas, ev.Err
 }
+
+// An io.Writer that will return os.EOF on the `n`th byte written
+type ErrWriter struct {
+	N int
+}
+
+func (e *ErrWriter) Write(p []byte) (n int, err os.Error) {
+	l := len(p)
+	e.N -= l
+	if e.N <= 0 {
+		return 0, os.EOF
+	}
+	return l, nil
+}
