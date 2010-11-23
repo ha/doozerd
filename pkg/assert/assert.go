9199b110228ebd4d5e39018263a4bcca286c33f5
Blake Mizerany <blake.mizerany@gmail.com>
2010-11-22 17:10:48 -0800

    assert: rename one letter vars

--- pkg/assert/assert.go
+++ pkg/assert/assert.go
@@ -8,8 +8,8 @@ import (
 	"fmt"
 )
 
-func assert(t *testing.T, b bool, f func(), cd int) {
-	if !b {
+func assert(t *testing.T, result bool, f func(), cd int) {
+	if !result {
 		_, file, line, _ := runtime.Caller(cd + 1)
 		t.Errorf("%s:%d", file, line)
 		f()
@@ -18,33 +18,33 @@ func assert(t *testing.T, b bool, f func(), cd int) {
 }
 
 func equal(t *testing.T, exp, got interface{}, cd int, args ...interface{}) {
-	f := func() {
+	fn := func() {
 		t.Errorf("!  Expected: %T %#v", exp, exp)
 		t.Errorf("!  Got:      %T %#v", got, got)
 		if len(args) > 0 {
 			t.Error("!", " -", fmt.Sprint(args...))
 		}
 	}
-	b := reflect.DeepEqual(exp, got)
-	assert(t, b, f, cd+1)
+	result := reflect.DeepEqual(exp, got)
+	assert(t, result, fn, cd+1)
 }
 
-func tt(t *testing.T, b bool, cd int, args ...interface{}) {
-	f := func() {
+func tt(t *testing.T, result bool, cd int, args ...interface{}) {
+	fn := func() {
 		t.Errorf("!  Failure")
 		if len(args) > 0 {
 			t.Error("!", " -", fmt.Sprint(args...))
 		}
 	}
-	assert(t, b, f, cd+1)
+	assert(t, result, fn, cd+1)
 }
 
-func T(t *testing.T, b bool, args ...interface{}) {
-	tt(t, b, 1, args...)
+func T(t *testing.T, result bool, args ...interface{}) {
+	tt(t, result, 1, args...)
 }
 
-func Tf(t *testing.T, b bool, format string, args ...interface{}) {
-	tt(t, b, 1, fmt.Sprintf(format, args...))
+func Tf(t *testing.T, result bool, format string, args ...interface{}) {
+	tt(t, result, 1, fmt.Sprintf(format, args...))
 }
 
 func Equal(t *testing.T, exp, got interface{}, args ...interface{}) {
@@ -56,19 +56,19 @@ func Equalf(t *testing.T, exp, got interface{}, format string, args ...interface
 }
 
 func NotEqual(t *testing.T, exp, got interface{}, args ...interface{}) {
-	f := func() {
+	fn := func() {
 		t.Errorf("!  Unexpected: <%#v>", exp)
 		if len(args) > 0 {
 			t.Error("!", " -", fmt.Sprint(args...))
 		}
 	}
-	b := !reflect.DeepEqual(exp, got)
-	assert(t, b, f, 1)
+	result := !reflect.DeepEqual(exp, got)
+	assert(t, result, fn, 1)
 }
 
-func Panic(t *testing.T, p interface{}, f func()) {
+func Panic(t *testing.T, err interface{}, fn func()) {
 	defer func() {
-		equal(t, p, recover(), 3)
+		equal(t, err, recover(), 3)
 	}()
-	f()
+	fn()
 }
