a9b3ef1c77255fe5fbb71ab9b3b172255472c601
Keith Rarick <kr@xph.us>
2010-11-29 13:14:14 -0800

    fill in more sample integration test

--- pkg/doozer_test.go
+++ pkg/doozer_test.go
@@ -1,6 +1,7 @@
 package doozer
 
 import (
+	"doozer/client"
 	"fmt"
 	"rand"
 	"testing"
@@ -22,8 +23,24 @@ func TestFoo(t *testing.T) {
 	a0, w := randAddr(), randAddr()
 	fmt.Println("web", w)
 	go Main("a", a0, "", w)
-	time.Sleep(1e9)
+	time.Sleep(1e8)
 	go Main("a", randAddr(), a0, "")
 	go Main("a", randAddr(), a0, "")
-	time.Sleep(3e9)
+	time.Sleep(1e8)
+
+	cl, err := client.Dial(a0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = cl.Noop()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// cl.Get("/doozer/members")
+	//for m in members {
+	//	cl.Get(/session/m)
+	//	cl.Get(/doozer/info/m/applied)
+	//}
 }
