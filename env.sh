39f535abc3ad1cf762fec7a48a5161366dc160cc
Blake Mizerany <blake.mizerany@gmail.com>
2010-12-02 21:48:20 -0800

    replace Makefile with all.sh

---
+++ env.sh
@@ -0,0 +1,22 @@
+REQS="
+    github.com/bmizerany/assert
+"
+
+PKGS="
+    util
+    exec
+    store
+    paxos
+    net
+    proto
+    lock
+    server
+    web
+    client
+    mon
+    test
+    timer
+    session
+    member
+    gc
+"
---
+++ test.sh
@@ -0,0 +1,16 @@
+if [ -f env.sh ]
+then . env.sh
+else
+    echo 1>&2 "! $0 must be run from the root directory"
+    exit 1
+fi
+
+mtest() {
+    cd pkg/$1
+    gomake test
+}
+
+(mtest)
+for pkg in $PKGS
+do (mtest $pkg)
+done
