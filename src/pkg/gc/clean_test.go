2e53e9d5f6a783a4c3e536ea8faf0f9caf42b68c
Keith Rarick <kr@xph.us>
2010-12-07 18:35:59 -0800

    gc: fix broken test (remove race)

--- src/pkg/gc/clean_test.go
+++ src/pkg/gc/clean_test.go
@@ -14,7 +14,13 @@ func TestGcClean(t *testing.T) {
 	st.Ops <- store.Op{1, store.Nop}
 	st.Ops <- store.Op{2, store.MustEncodeSet("/doozer/slot/1", "a", store.Missing)}
 	st.Ops <- store.Op{3, store.MustEncodeSet("/doozer/info/a/applied", "2", store.Missing)}
-	st.Ops <- store.Op{4, store.Nop}
+
+	st.Ops <- store.Op{4, store.MustEncodeSet("/doozer/info/X/applied", "0", store.Missing)}
+	ch := st.Watch("/x")
+	st.Ops <- store.Op{5, store.MustEncodeSet("/x", "", store.Missing)}
+	<-ch
+	close(ch)
+
 	ev := <-st.Wait(1)
 	assert.Equal(t, store.ErrTooLate, (ev).Err)
 }
