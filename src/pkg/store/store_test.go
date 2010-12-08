ee8546dcbfc655faa005733736a7e9b958fbac39
Blake Mizerany <blake.mizerany@gmail.com>
2010-12-07 23:40:49 -0800

    store: note another test race

--- src/pkg/store/store_test.go
+++ src/pkg/store/store_test.go
@@ -1055,6 +1055,8 @@ func TestGetDirAndWatch(t *testing.T) {
 	st.Ops <- Op{4, mut4}
 	st.Sync(4)
 
+	// !  Expected: store.Event store.Event{Seqn:0x0, Path:"/x/a", Body:"1", Cas:"1", Mut:"", Err:<nil>, Getter:<nil>}
+	// !  Got:      store.Event store.Event{Seqn:0x2, Path:"/x/b", Body:"2", Cas:"2", Mut:":/x/b=2", Err:<nil>, Getter:<nil>}
 	assert.Equal(t, Event{0, "/x/a", "1", "1", "", nil, nil}, clearGetter(<-ch))
 	assert.Equal(t, Event{2, "/x/b", "2", "2", mut2, nil, nil}, clearGetter(<-ch))
 	assert.Equal(t, Event{4, "/x/c", "3", "4", mut4, nil, nil}, clearGetter(<-ch))
