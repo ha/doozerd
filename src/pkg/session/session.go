17f666b54a7dd389204aa32d91eb423ed59f61c4
Keith Rarick <kr@xph.us>
2011-01-26 14:56:19 -0800

    session: use original timer cas token

--- src/pkg/session/session.go
+++ src/pkg/session/session.go
@@ -11,7 +11,6 @@ var sessions = store.MustCompileGlob("/session/*")
 func Clean(s *store.Store, p paxos.Proposer) {
 	timer := timer.New(sessions, timer.OneSecond, s)
 	for tick := range timer.C {
-		_, cas := s.Get(tick.Path)
-		paxos.Del(p, tick.Path, cas, nil)
+		paxos.Del(p, tick.Path, tick.Cas, nil)
 	}
 }
