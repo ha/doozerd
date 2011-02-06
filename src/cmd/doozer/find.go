e769f5e9198f914727119d29a3e316f3f818afa8
Blake Mizerany <blake.mizerany@gmail.com>
2011-02-05 19:10:04 -0800

    first pass at find(1)

---
+++ src/cmd/doozer/find.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"doozer/client"
+	"fmt"
+    "os"
+)
+
+
+func init() {
+	cmds["find"] = cmd{find, "<glob>", "list files"}
+	cmdHelp["find"] = `Prints the tree matching <glob>
+
+Rules for <glob> pattern-matching:
+ - '?' matches a single char in a single path component
+ - '*' matches zero or more chars in a single path component
+ - '**' matches zero or more chars in zero or more components
+ - any other sequence matches itself
+
+Prints a sequence of paths, one for each file/directory. Format of each record:
+
+  <path> LF
+
+Here, <path> is the file's path, and LF is an ASCII line-feed char.
+`
+}
+
+
+func find(glob string) {
+	c := client.New("<test>", *addr)
+
+	if glob[len(glob)-1:] != "/" {
+		glob = glob+"/"
+	}
+
+	w, err := c.Walk(glob+"**", 0)
+	if err != nil {
+		bail(err)
+	}
+
+    for ev := range w.C {
+        if ev.Err != nil {
+            fmt.Fprintln(os.Stderr, ev.Err)
+        }
+
+        fmt.Println(ev.Path)
+    }
+}
