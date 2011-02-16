21bdfc94eedd0f141cc3a51ac87966d36afe3796
Keith Rarick <kr@xph.us>
2011-02-16 12:04:41 -0800

    import type sink

---
+++ src/pkg/consensus/sink.go
@@ -0,0 +1,18 @@
+package consensus
+
+type sink struct {
+	v    string
+	done bool
+}
+
+func (s *sink) Put(m *M) {
+	if s.done {
+		return
+	}
+
+	switch m.Cmd() {
+	case M_LEARN:
+		s.done, s.v = true, string(m.Value)
+	}
+	return
+}
---
+++ src/pkg/consensus/sink_test.go
@@ -0,0 +1,37 @@
+package consensus
+
+import (
+	"github.com/bmizerany/assert"
+	"testing"
+)
+
+func TestSinkLearnsAValue(t *testing.T) {
+	var s sink
+
+	s.Put(newLearn("foo"))
+	assert.Equal(t, true, s.done)
+	assert.Equal(t, "foo", s.v)
+}
+
+func TestSinkLearnsOkSticky(t *testing.T) {
+	var s sink
+
+	s.Put(newLearn("foo"))
+	assert.Equal(t, true, s.done)
+	assert.Equal(t, "foo", s.v)
+
+	s.Put(newLearn("bar"))
+	assert.Equal(t, true, s.done)
+	assert.Equal(t, "foo", s.v)
+}
+
+func TestSinkIgnoresOtherMessages(t *testing.T) {
+	var s sink
+
+	s.Put(newVote(1, "foo"))
+	assert.Equal(t, false, s.done)
+
+	s.Put(newLearn("foo"))
+	assert.Equal(t, true, s.done)
+	assert.Equal(t, "foo", s.v)
+}
