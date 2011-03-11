512ffa25f0d10aa79614abd93dad2f57c253a8b2
Keith Rarick <kr@xph.us>
2011-03-11 02:03:52 -0800

    refactor

--- src/pkg/gc/pulse.go
+++ src/pkg/gc/pulse.go
@@ -9,13 +9,14 @@ import (
 )
 
 func Pulse(node string, seqns <-chan int64, p consensus.Proposer, sleep int64) {
+	path := "/doozer/info/" + node + "/applied"
 	for {
 		seqn := strconv.Itoa64(<-seqns)
 		if closed(seqns) {
 			break
 		}
 
-		e := consensus.Set(p, "/doozer/info/"+node+"/applied", []byte(seqn), store.Clobber)
+		e := consensus.Set(p, path, []byte(seqn), store.Clobber)
 		if e.Err != nil {
 			log.Println(e.Err)
 		}
