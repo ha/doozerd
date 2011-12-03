129708de71163553bf5b45cd9b451279242c22f6
Aram Hăvărneanu <aram.h@mgk.ro>
2011-12-03 20:41:21 +0200

    logfs: better documentation

--- logfs.go
+++ logfs.go
@@ -5,8 +5,8 @@ import "os"
 
 // A Record is the fundamental unit you read, write or delete.
 type Record struct {
-	Data []byte
-	Seqn uint64
+	Data []byte // data to set or retrieve.
+	Seqn uint64 // Sequence number, each I/O operation has one.
 }
 
 // Logfs is used for issuing I/O to the backing file.
