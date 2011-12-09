efe5b4bd944c587048a9e49b06bf785d41abbfa2
Aram Hăvărneanu <aram.h@mgk.ro>
2011-12-10 00:11:51 +0200

    logfs: field reordering in blockHeader, better documentation.

--- disk.go
+++ disk.go
@@ -11,13 +11,12 @@ type block struct {
 }
 
 // blockHeader describes a block.  A block is variable sized, so this
-// structure is fixed size to be read with a single read call before
-// reading the actual data.
+// header is fixed size to know how much to read.
 type blockHeader struct {
 	headerSum  uint64 // Own checksum.
 	cookie     uint64 // To be matched with previous block.
 	nextCookie uint64 // Next block's cookie must match this.
-	dataSum    uint64 // block.data checksum.
 	dataLen    uint64 // block.data length.
+	dataSum    uint64 // block.data checksum.
 	seqn       uint64 // Sequence number, must grow in the list.
 }
