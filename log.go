dd562d6d510431b4e5fd939e78e18625de9d3765
Aram Hăvărneanu <aram.h@mgk.ro>
2011-12-26 17:55:17 +0200

    log: documentation.

--- log.go
+++ log.go
@@ -1,1 +1,6 @@
+// Package log implements a persistence layer for doozer using a circular log.
+// Writes containing mutations are issued at the end of the log.  Deletions
+// are garbage collected from the head of the logso that the backing file
+// does not grow indefinetly.
+
 package log
