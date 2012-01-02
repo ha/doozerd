569dc540ff7214277a26d85c25ffd0cec0e63cd1
Aram Hăvărneanu <aram.h@mgk.ro>
2012-01-02 23:13:36 +0200

    persistence: implemented iops, Store, Retrieve and block convenience functions.

--- block.go
+++ block.go
@@ -5,26 +5,69 @@ import (
 	"io"
 )
 
+// blocks are written to disk.
 type block struct {
 	hdr  blockHdr // header describing data.
 	data []byte   // payload.
 }
 
+// blockHdr describes a block.
 type blockHdr struct {
-	score []byte // SHA-1(block.data), 20 bytes.
-	size  int    // len(block.data).
+	score [20]byte // SHA-1(block.data).
+	size  int      // len(block.data).
 }
 
+// newBlock returns a block created from mutation.
 func newBlock(mutation string) (b *block) {
-	b.data = []byte(mutation)
 	b.hdr.size = len(b.data)
+
 	sha1 := sha1.New()
 	io.WriteString(sha1, mutation)
-	b.hdr.score = sha1.Sum(nil)
+	scoreCpy(&b.hdr.score, sha1.Sum(nil))
+
+	b.data = []byte(mutation)
 
 	return
 }
 
-func (b block)String() string {
+// scoreCpy copied sum into score.
+func scoreCpy(score *[20]byte, sum []byte) {
+	if len(score) != len(sum) {
+		panic("checksum length mismatch")
+	}
+
+	for i := 0; i < len(score); i++ {
+		(*score)[i] = sum[i]
+	}
+}
+
+// scoreCmp does a C-style compare between sum and score.
+func scoreCmp(score *[20]byte, sum []byte) byte {
+	if len(score) != len(sum) {
+		panic("checksum length mismatch")
+	}
+
+	for i := 0; i < len(score); i++ {
+		r := (*score)[i] - sum[i]
+		if r != 0 {
+			return r
+		}
+	}
+
+	return 0
+}
+
+func (b block) String() string {
 	return string(b.data)
+}
+
+// isValid validates the checksum of a block.
+func (b block) isValid() bool {
+	sha1 := sha1.New()
+	sha1.Write(b.data[:b.hdr.size])
+	if scoreCmp(&b.hdr.score, sha1.Sum(nil)) != 0 {
+		return false
+	}
+
+	return true
 }
--- persistence.go
+++ persistence.go
@@ -6,17 +6,24 @@
 package persistence
 
 import (
+	"encoding/binary"
+	"errors"
 	"io"
 )
 
 // Journal represents a file where doozer can save state.
 type Journal struct {
-	rw io.ReadWriter // arena
+	r chan *iop // reads are issued here, pointer b.c. user modifies it.
+	w chan iop  // writes are issued here.
+	q chan bool // quit signal.
 }
 
+// iop represents an I/O request.  err is used to signal the result back
+// to the client.  mut is filled by the client, in case of writes and
+// by the system in case of reads.
 type iop struct {
 	mut string
-	c   chan error
+	err chan error
 }
 
 // NewJournal opens the named file for synchronous I/O, creating it
@@ -24,23 +31,87 @@ type iop struct {
 // If successful, methods on the returned Journal can be used for I/O.
 // It returns a Journal and an error, if any.
 func NewJournal(name string) (j *Journal, err error) {
-	j.rw, err = newArena(name)
+	j.r = make(chan *iop)
+	j.w = make(chan iop)
+	j.q = make(chan bool)
+	arena, err := newArena(name)
 	if err != nil {
 		return nil, err
 	}
+	go iops(arena, j)
 	return
 }
 
 // Store writes the mutation to the Journal.
 func (j *Journal) Store(mutation string) error {
-	panic("not implemented")
-	return nil
+	wrq := iop{mutation, make(chan error)}
+	j.w <- wrq
+	err := <-wrq.err
+
+	return err
 }
 
 // Retrieve reads the next mutation from the Journal.  It returns
 // the mutation and an error, if any.  EOF is signaled by a nil
 // mutation with err set to io.EOF
 func (j *Journal) Retrieve() (mutation string, err error) {
-	panic("not implemented")
-	return "", nil
+	rrq := iop{err: make(chan error)}
+	j.r <- &rrq
+	err = <-rrq.err
+
+	return rrq.mut, err
+}
+
+// iops sits in a loop and processes requests sent by Store and Retrieve.
+// Clients of this function specify a channel where it can send back
+// the result of the operation. 
+func iops(rw io.ReadWriter, j *Journal) {
+	for {
+		select {
+		// read.
+		case rop := <-j.r:
+			b := block{}
+
+			// Read the header so we know how much to read next.
+			err := binary.Read(rw, binary.LittleEndian, &b.hdr)
+
+			// If everything was fine, we can read the data.
+			if err == nil {
+				_, err = rw.Read(b.data)
+			}
+
+			// We need to make sure the checksum is valid.
+			if !b.isValid() {
+				err = errors.New("read an invalid block")
+			}
+
+			// Everything went well, copy the data to the client supplied
+			// structure and signal the client we are done.
+			rop.mut = b.String()
+			rop.err <- err
+
+		// write.
+		case wop := <-j.w:
+			b := newBlock(wop.mut)
+
+			// We use two write calls bacause we use encoding/binary
+			// to write the fixed length header.
+
+			err := binary.Write(rw, binary.LittleEndian, b.hdr)
+
+			// We'we written the header successfully, write the reast
+			// of the data.
+			if err == nil {
+				_, err = rw.Write(b.data)
+			}
+
+			// Everything went well, we signal the user we are done.
+			wop.err <- err
+
+		// quit.
+		case <-j.q:
+			return
+		}
+	}
+	return
 }
