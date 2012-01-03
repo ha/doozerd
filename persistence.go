4a22c60bf83a55287bd4b0f331d1c0131f966851
Aram Hăvărneanu <aram.h@mgk.ro>
2012-01-03 18:11:53 +0200

    persistence: iops is only a demuxer, functionality is delegated to encodedRead and decodedWrite.

--- persistence.go
+++ persistence.go
@@ -68,47 +68,14 @@ func (j *Journal) Retrieve() (mutation string, err error) {
 func iops(rw io.ReadWriter, j *Journal) {
 	for {
 		select {
-		// read.
 		case rop := <-j.r:
-			b := block{}
-
-			// Read the header so we know how much to read next.
-			err := binary.Read(rw, binary.LittleEndian, &b.hdr)
-
-			// If everything went fine, we can read the data.
-			if err == nil {
-				_, err = io.ReadAtLeast(rw, b.data, b.hdr.size)
-			}
-
-			// We need to make sure the checksum is valid.
-			if !b.isValid() {
-				err = errors.New("read an invalid block")
-			}
-
-			// Everything went well, copy the data to the client supplied
-			// structure and signal the client we are done.
-			rop.mut = b.String()
+			mut, err := decodedRead(rw)
+			rop.mut = mut
 			rop.err <- err
 
-		// write.
 		case wop := <-j.w:
-			b := newBlock(wop.mut)
+			wop.err <- encodedWrite(rw, wop.mut)
 
-			// We use two write calls bacause we use encoding/binary
-			// to write the fixed length header.
-
-			err := binary.Write(rw, binary.LittleEndian, b.hdr)
-
-			// We'we written the header successfully, write the reast
-			// of the data.
-			if err == nil {
-				_, err = rw.Write(b.data)
-			}
-
-			// Everything went well, we signal the user we are done.
-			wop.err <- err
-
-		// quit.
 		case <-j.q:
 			return
 		}
@@ -116,3 +83,45 @@ func iops(rw io.ReadWriter, j *Journal) {
 	return
 }
 
+// decodedRead reads a block from the reader, decodes it into a
+// mutation and returns it along with an error.
+func decodedRead(r io.Reader) (mut string, err error) {
+	b := block{}
+
+	// Read the header so we know how much to read next.
+	err = binary.Read(r, binary.LittleEndian, &b.hdr)
+	if err != nil {
+		return
+	}
+
+	// If everything went fine, we can read the data.
+	_, err = io.ReadAtLeast(r, b.data, b.hdr.size)
+	
+	// We need to make sure the checksum is valid.
+	if !b.isValid() {
+		err = errors.New("read an invalid block")
+		return
+	}
+	
+	mut = b.String()
+	return
+}
+
+// encodedRead encodes a mutation into a block and writes the
+// block to the writer returning an error.
+func encodedWrite(w io.Writer, mut string) (err error) {
+	b := newBlock(mut)
+
+	// We use two write calls bacause we use encoding/binary
+	// to write the fixed length header.
+
+	err = binary.Write(w, binary.LittleEndian, b.hdr)
+	if err != nil {
+		return
+	}
+
+	// We'we written the header successfully, write the rest
+	// of the data.
+	_, err = w.Write(b.data)
+	return
+}
