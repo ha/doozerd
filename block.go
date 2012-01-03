6f4968c9f14eea5e5513b14bb8dd6f9403a12e50
Aram Hăvărneanu <aram.h@mgk.ro>
2012-01-03 18:51:06 +0200

    persistence: removed scoreCmp and scoreCpy, replaced with bytes.Equal() and copy().

--- block.go
+++ block.go
@@ -1,6 +1,7 @@
 package persistence
 
 import (
+	"bytes"
 	"crypto/sha1"
 	"io"
 )
@@ -23,44 +24,11 @@ func newBlock(mutation string) (b *block) {
 
 	sha1 := sha1.New()
 	io.WriteString(sha1, mutation)
-	scoreCpy(b.hdr.score[:], sha1.Sum(nil))
+	copy(b.hdr.score[:], sha1.Sum(nil))
 
 	b.data = []byte(mutation)
 
 	return
-}
-
-// scoreCpy copies sum into score.
-func scoreCpy(score, sum []byte) {
-	if len(score) != len(sum) {
-		panic("checksum length mismatch")
-	}
-
-	for i, _ := range score {
-		score[i] = sum[i]
-	}
-}
-
-// scoreCmp does a C-style compare between sum and score.
-func scoreCmp(score, sum []byte) int {
-	switch {
-	case len(score) > len(sum):
-		return 1
-	case len(score) < len(sum):
-		return -1
-
-	}
-
-	for i, _ := range score {
-		switch {
-		case score[i] > sum[i]:
-			return 1
-		case score[i] < sum[i]:
-			return -1
-		}
-	}
-
-	return 0
 }
 
 func (b block) String() string {
@@ -71,9 +39,5 @@ func (b block) String() string {
 func (b block) isValid() bool {
 	sha1 := sha1.New()
 	sha1.Write(b.data[:b.hdr.size])
-	if scoreCmp(b.hdr.score[:], sha1.Sum(nil)) != 0 {
-		return false
-	}
-
-	return true
+	return bytes.Equal(b.hdr.score[:], sha1.Sum(nil))
 }
