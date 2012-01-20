3282f31d29d442d676e9606d8d1daab92639526f
Aram Hăvărneanu <aram.h@mgk.ro>
2012-01-20 23:44:31 +0100

    imported persistence.

---
+++ persistence/journal.go
@@ -0,0 +1,110 @@
+// Package persistence allows doozer to save state across server restarts.
+// Doozer mutations are apended to a file on disk.  When a file reaches
+// a maximum size a new file is used.  Old files are sometimes
+// rewritten to prune deleted mutations.
+
+package persistence
+
+import (
+	"encoding/binary"
+	"errors"
+	"io"
+	"os"
+	"sync"
+)
+
+// Journal represents a file where doozer can save state.  Doozer usally
+// uses a list of multiple journals.
+type Journal struct {
+	mutex sync.Mutex // each mutation must be read/written atomically.
+	rw    *os.File   // mutations are read/written from here.
+}
+
+// NewJournal opens the named file for synchronous I/O, creating it
+// with mode 0640 if it does not exist and prepares it for logging operation.
+// If successful, methods on the returned Journal can be used for I/O.
+// It returns a Journal and an error, if any.
+func NewJournal(name string) (j *Journal, err error) {
+	// File is created if it does not exist, file must be opened synchronously
+	// in order to guarantee consistency, file is group readable in order
+	// to be read by an administrator if doozer is ran by its own user, file
+	// is append-only because data is never overwritten.
+	f, err := os.OpenFile(name, os.O_APPEND|os.O_CREATE|os.O_RDWR|os.O_SYNC, 0640)
+	if err != nil {
+		return
+	}
+
+	j = &Journal{rw: f}
+	return
+}
+
+// Store writes the mutation to the Journal.
+func (j Journal) WriteMutation(m string) (err error) {
+	j.mutex.Lock()
+	defer j.mutex.Unlock()
+	return writeMutation(j.rw, m)
+}
+
+// Retrieve reads the next mutation from the Journal.  It returns
+// the mutation and an error, if any.  EOF is signaled by a nil
+// mutation with err set to io.EOF
+func (j Journal) ReadMutation() (m string, err error) {
+	j.mutex.Lock()
+	defer j.mutex.Unlock()
+	return readMutation(j.rw)
+}
+
+// Close shuts down the journal.
+func (j Journal) Close() {
+	j.mutex.Lock()
+	defer j.mutex.Unlock()
+	j.rw.Close()
+}
+
+// readMutation reads a block from the reader, decodes it into a
+// mutation and returns it along with an error.
+func readMutation(r io.Reader) (mut string, err error) {
+	b := block{}
+
+	// Read the header so we know how much to read next.
+	err = binary.Read(r, binary.LittleEndian, &b.Hdr)
+	if err != nil {
+		return
+	}
+
+	// If everything went fine, we can read the data.
+	b.Data = make([]byte, int(b.Hdr.Size))
+	_, err = io.ReadFull(r, b.Data)
+	if err != nil {
+		return
+	}
+
+	// We need to make sure the checksum is valid.
+	if b.isValid() != true {
+		err = errors.New("checksum check failed")
+		return
+	}
+
+	mut = string(b.Data)
+	return
+}
+
+// writeMutation encodes a mutation into a block and appends the
+// block to the writer returning an error. Seek offset is unmodified.
+func writeMutation(w io.WriteSeeker, mut string) (err error) {
+	b := newBlock(mut)
+
+	offset, _ := w.Seek(0, 1) // remember where we are.
+	// We use two write calls bacause we use encoding/binary
+	// to write the fixed length header.
+	err = binary.Write(w, binary.LittleEndian, b.Hdr)
+	if err != nil {
+		return
+	}
+
+	// We'we written the header successfully, write the rest
+	// of the data.
+	_, err = w.Write(b.Data)
+	w.Seek(offset, 0) // rewind so we don't break readMutation(). 
+	return
+}
---
+++ persistence/journal_test.go
@@ -0,0 +1,125 @@
+package persistence
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"testing"
+	"time"
+)
+
+// Test data stolen from crypto/sha1/sha1_test.go.
+var testData = []string{
+	"",
+	"a",
+	"ab",
+	"abc",
+	"abcd",
+	"abcde",
+	"abcdef",
+	"abcdefg",
+	"abcdefgh",
+	"abcdefghi",
+	"abcdefghij",
+	"Discard medicine more than two years old.",
+	"He who has a shady past knows that nice guys finish last.",
+	"I wouldn't marry him with a ten foot pole.",
+	"Free! Free!/A trip/to Mars/for 900/empty jars/Burma Shave",
+	"The days of the digital watch are numbered.  -Tom Stoppard",
+	"Nepal premier won't resign.",
+	"For every action there is an equal and opposite government program.",
+	"His money is twice tainted: 'taint yours and 'taint mine.",
+	"There is no reason for any individual to have a computer in their home. -Ken Olsen, 1977",
+	"It's a tiny change to the code and not completely disgusting. - Bob Manchek",
+	"size:  a.out:  bad magic",
+	"The major problem is with sendmail.  -Mark Horton",
+	"Give me a rock, paper and scissors and I will move the world.  CCFestoon",
+	"If the enemy is within range, then so are you.",
+	"It's well we cannot hear the screams/That we create in others' dreams.",
+	"You remind me of a TV show, but that's all right: I watch it anyway.",
+	"C is as portable as Stonehedge!!",
+	"Even if I could be Shakespeare, I think I should still choose to be Faraday. - A. Huxley",
+	"The fugacity of a constituent in a mixture of gases at a given temperature is proportional to its mole fraction.  Lewis-Randall Rule",
+	"How can you write a big system without C++?  -Paul Glick",
+}
+
+var testFileSha1 = "2c9e98acf63756007d3e3bbdcc4d80882e06a2aa"
+
+func TestWriteMutation(t *testing.T) {
+	f, err := ioutil.TempFile("", "journal")
+	if err != nil {
+		t.Fatal(err)
+	}
+	name := f.Name()
+	f.Close()
+	defer os.Remove(name)
+
+	j, err := NewJournal(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, v := range testData {
+		err = j.WriteMutation(v)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	j.Close()
+
+	b, err := ioutil.ReadFile(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sha1 := sha1.New()
+	sha1.Write(b)
+	s := fmt.Sprintf("%x", sha1.Sum(nil))
+	if s != testFileSha1 {
+		t.Fatal("journal file has an unexpected SHA-1 hash")
+	}
+}
+
+func TestReadMutation(t *testing.T) {
+	f, err := ioutil.TempFile("", "journal")
+	if err != nil {
+		t.Fatal(err)
+	}
+	name := f.Name()
+	f.Close()
+	defer os.Remove(name)
+
+	// Generate some random data.
+	randData := make([]string, 128)
+	r := rand.New(rand.NewSource(int64(time.Now().Nanosecond())))
+	for i, _ := range randData {
+		tmp := make([]byte, r.Intn(2<<16))
+		for j := range tmp {
+			tmp[j] = byte(r.Intn(256))
+		}
+		randData[i] = string(tmp)
+	}
+	// Append it to the static data.
+	testData = append(testData, randData...)
+
+	j, err := NewJournal(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, v := range testData {
+		err = j.WriteMutation(v)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	for _, v := range testData {
+		m, err := j.ReadMutation()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if m != v {
+			t.Fatalf("read from store '%v', should be '%v'", m, v)
+		}
+	}
+	j.Close()
+}
