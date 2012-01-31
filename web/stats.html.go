610f551c5652a8d925e6777083375af3915315f1
Blake Mizerany <blake.mizerany@gmail.com>
2012-01-31 15:11:17 -0800

    web: generated files are generated from ./

--- web/stats.html.go
+++ web/stats.html.go
@@ -1,5 +1,5 @@
 package web
 
-// This file was generated from stats.html.
+// This file was generated from web/stats.html.
 
 var stats_html string = "<html>\n  <body>\n    <table>\n      <tr>\n        <th>\n        <th>Alloc\n        <th>TotalAlloc\n      </tr>\n      <tr>\n        <td>\n        <td>{Alloc}\n        <td>{TotalAlloc}\n      </tr>\n    </table>\n  </body>\n</html>\n"
