b247b7cc206fe01abe09d3b1f5c9d73064369e5a
Aram Hăvărneanu <aram.h@mgk.ro>
2012-01-31 22:00:52 +0100

    web: added generated files into the repository.

---
+++ web/stats.html.go
@@ -0,0 +1,5 @@
+package web
+
+// This file was generated from stats.html.
+
+var stats_html string = "<html>\n  <body>\n    <table>\n      <tr>\n        <th>\n        <th>Alloc\n        <th>TotalAlloc\n      </tr>\n      <tr>\n        <td>\n        <td>{Alloc}\n        <td>{TotalAlloc}\n      </tr>\n    </table>\n  </body>\n</html>\n"
