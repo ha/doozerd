61b6b5b33182b9013abead5c97700863a6485b3c
Matt Reiferson <mreiferson@gmail.com>
2013-03-04 19:51:51 -0500

    make note of potential deadlock issues

--- quiet/quiet.go
+++ quiet/quiet.go
@@ -6,6 +6,11 @@ import (
 )
 
 func init() {
+	// TODO: if this is switched to ioutil.Discard a few of the tests hang.
+	// Presumably this means that there are legitimate deadlock issues 
+	// somewhere in the core...
+	// OSX 10.8.2 go1.0.3
+	// (GOMAXPROCS > 1 also resolves the issue indicating the same)
 	dn, err := os.OpenFile(os.DevNull, 0, 0)
 	if err != nil {
 		panic(err)
